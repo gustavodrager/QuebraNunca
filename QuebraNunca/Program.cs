b17254c63d59a5d9e7185b3b031e7dea5b93af47
Gustavo Drager <gustavodrager@gmail.com>
2020-11-10 10:00:29 -0300

    primeiro

---
+++ QuebraNunca/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace QuebraNunca
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+            Atleta atleta = new Atleta();
+
+            atleta.nome = "Gustavo";
+            atleta.lado = "esquerdo";
+            atleta.saldo = 100;
+
+            Console.WriteLine(atleta.nome);
+            Console.WriteLine(atleta.lado);
+            Console.WriteLine(atleta.saldo);
+
+            Console.WriteLine(atleta.nome == atleta.lado);
+
+            atleta.Debitar(50);
+
+            Console.WriteLine(atleta.saldo);
+
+            atleta.Creditar(100);
+
+            Console.WriteLine(atleta.saldo);
+
+            Console.ReadLine();
+        }
+    }
+}
